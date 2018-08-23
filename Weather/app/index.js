999b539f4129076a0c97b29bf532b7747ef93823
Pham Quy Hai <me@haipq.com>
2018-08-23 17:03:36 +0700

    Add design

--- Weather/app/index.js
+++ Weather/app/index.js
@@ -7,14 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { StyleSheet, Text, View } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -23,7 +16,7 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>sdfsdfds</Text>
       </View>
     );
   }
